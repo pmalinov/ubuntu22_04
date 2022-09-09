b664133618512283c6c026edf303f3341b86becd
pmalinov <105856748+pmalinov@users.noreply.github.com>
2022-09-09 23:28:54 +0200

    Update 0_Start_VNC.sh

--- 0_Start_VNC.sh
+++ 0_Start_VNC.sh
@@ -5,6 +5,11 @@
  #
  ####################################
 
+################################################################
+# command for set pass
+#                             x11vnc -storepasswd
+# and insert # mypassword
+###############################################################
 # start x11vnc # -forever
 sleep 2
-echo "RutPassword" | sudo -S x11vnc -rfbauth /home/dell/.vnc/passwd -rfbport 5901 -forever
+echo "root_passoword" | sudo -S x11vnc -rfbauth /home/dell/.vnc/passwd -rfbport 5901 -forever
